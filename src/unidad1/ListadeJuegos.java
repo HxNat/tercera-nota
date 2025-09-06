bf2e269d28188cbffb4f86b106ed691878b6b3ea
HxNat <nangaritar@ufpso.edu.co>
2025-09-05 21:22:58 -0500

    Actualizacion de ejercicios practicos

---
+++ src/unidad1/ListadeJuegos.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ListadeJuegos {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("¿Cuantos juegos desea agregar?");
+            int cantidadJuegos = scanner.nextInt();
+            scanner.nextLine();
+
+            String[] juegos = new String[cantidadJuegos];
+
+            for (int i = 0; i < cantidadJuegos; i++) {
+                System.out.println("Ingrese el nombre del juego " + (i + 1) + ":");
+                juegos[i] = scanner.nextLine();
+            }
+
+            System.out.println("Lista de juegos ingresados:");
+            for (int i = 0; i < juegos.length; i++) {
+                System.out.println((i + 1) + ". " + juegos[i]);
+            }
+        }
+    }
+}
---
+++ src/unidad1/ResetScanner1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ResetScanner1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int num1 = sc.nextInt();
+        System.out.println("Número leído: " + num1);
+
+        // "Reseteamos" creando un nuevo objeto
+        sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese otro número: ");
+        int num2 = sc.nextInt();
+        System.out.println("Número leído: " + num2);
+
+        sc.close();
+    }
+}
